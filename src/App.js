31370abe88ae84b185f34d36f720333b219ce60c
Sumant Shah <sumant.shah@gigacars.com>
2023-12-07 22:01:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './app.css';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <img className='back' src="images/back4.jpg" alt="" />
+        <img className='girl' src="images/girl-removebg-preview.png" alt="" />
+        <img className='lo1' src="images/lo1.png" alt="" />
+        <img className='lo2' src="images/lo2.png" alt="" />
+        <img className="lo3" src="images/lo3.png" alt="" />
+        <img className="lo4" src="images/lo4.png" alt="" />
+        <img className="love1" src="images/love2.png" alt="" />
+        <img className="love2" src="images/love1.png" alt="" />
+        <img className="love3" src="images/love2.png" alt="" />
+        <img className="boy" src="images/boy.png" alt="" />
+        <img className="ballon" src="images/ballon-removebg-preview.png" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default App;
